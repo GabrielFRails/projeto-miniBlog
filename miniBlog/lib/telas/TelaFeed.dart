b7ff9dcbd1d7c82717bee4a171451bfe59cefac5
kalheeso <matheusgerlo@gmail.com>
2021-08-13 15:20:44 -0300

    listview aguardando endpoint

--- miniBlog/lib/telas/TelaFeed.dart
+++ miniBlog/lib/telas/TelaFeed.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:miniBlog/widgets_padrao/TestePosts.dart';
+import 'package:get_it/get_it.dart';
+import 'package:miniBlog/controladores/ControladorUsuario.dart';
+import 'package:miniBlog/widgets_padrao/PostagemWidget.dart';
 
 class TelaFeed extends StatefulWidget {
   const TelaFeed({Key key}) : super(key: key);
@@ -9,6 +11,7 @@ class TelaFeed extends StatefulWidget {
 }
 
 class _TelaFeedState extends State<TelaFeed> {
+  ControladorUsuario _controladorUsuario = GetIt.I.get<ControladorUsuario>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,16 +22,19 @@ class _TelaFeedState extends State<TelaFeed> {
   Widget listaDePosts() {
     return Container(
       color: Colors.white,
-      child: ListView.separated(
-        itemBuilder: (BuildContext context, int index) {
-          return posts[index];
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          var postSeguido = _controladorUsuario.postsSeguidos[index];
+          return PostagemWidget(
+              avatar: null,
+              username: null,
+              name: null,
+              timeAgo: null,
+              text: null,
+              comments: null,
+              favorites: null);
         },
-        separatorBuilder: (BuildContext context, int index) => Divider(
-          indent: 12,
-          endIndent: 12,
-          thickness: 1,
-        ),
-        itemCount: posts.length,
+        itemCount: _controladorUsuario.postsSeguidos.length,
       ),
     );
   }
